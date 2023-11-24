f36253cccf307e241e4a0383b7adcc5b909ada7b
innasz <mini5167@naver.com>
2023-11-24 16:29:22 +0900

    feat: 초기 세팅

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex justify-between">
+      <h1>MBTI TEST</h1>
+      <h1>앱으로 보기</h1>
+    </div>
+  );
+}
+
+export default Header;
